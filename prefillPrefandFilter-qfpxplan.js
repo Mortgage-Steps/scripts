094a4d6e3aa4bae824a1952ea5289b1d02fd182b
Joshua Noakes <joshua.noakes1@icloud.com>
2023-11-28 09:48:38 +0000

    base qfp pref script

---
+++ prefillPrefandFilter-qfpxplan.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         QFP - Xplan Default No Preferences and Filters
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Default all options within the Preferences and Filters on QFP - Xplan
+// @author       Kira https://github.com/Joshua-Noakes1
+// @match        https://quilter.xplan-mortgage.iress.co.uk/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=iress.co.uk
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
